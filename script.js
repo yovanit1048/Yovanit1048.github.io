eff9ecb2ac4722b8da517f01f8be7db941e0f2e6
yovanit1048 <56267054+yovanit1048@users.noreply.github.com>
2019-10-08 11:52:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "YovaniTellez"
+let userName = "Yovanit1048";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
